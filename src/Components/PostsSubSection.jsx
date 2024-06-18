6542be3bcf4169bcd46da2280f6a525c91daea0f
Edgar767 <utm20040079@utma.edu.mx>
2024-06-18 13:02:17 -0600

    Nuevos Cambios

--- src/Components/PostsSubSection.jsx
+++ src/Components/PostsSubSection.jsx
@@ -12,7 +12,7 @@ const PostsSubSection = ({ posts }) => {
                                         <img src={items.img} loading="lazy" alt={items.title} className="w-full rounded-lg" />
                                         <div className="mt-3 space-y-2">
                                             <span className="block text-indigo-600 text-sm">{items.date}</span>
-                                            <h3 className="text-lg text-gray-800 duration-150 group-hover:text-indigo-600 font-semibold">
+                                            <h3 className="text-lg text-gray-800 duration-150 group-hover:text-indigo-600 font-extrabold">
                                                 {items.title}
                                             </h3>
                                             <p className="text-gray-600 text-sm duration-150 group-hover:text-gray-800">{items.desc}</p>
