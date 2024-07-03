91a48dac9cb673b52c61a982b501a0f9482deb10
Edgar767 <utm20040079@utma.edu.mx>
2024-07-03 11:06:50 -0600

    SearchBar ahora es responsive

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -8,18 +8,18 @@ const SearchBar = ({ searchTerm, handleSearchChange, activeTab, handleFilterChan
   };
 
   return (
-    <div className="flex items-center gap-4">
-      <label className="input input-bordered flex items-center gap-2 ml-4 rounded-full overflow-hidden">
+    <div className="flex flex-col sm:flex-row items-center gap-4 w-full px-4 py-2">
+      <label className="input input-bordered flex items-center gap-2 rounded-full overflow-hidden w-full sm:w-auto mb-2 sm:mb-0">
         <input
           type="text"
-          className="grow text-sm rounded-full outline-none"
+          className="grow text-sm rounded-full outline-none w-full"
           placeholder="Buscar"
           value={searchTerm}
           onChange={handleSearchChange}
         />
       </label>
       <select 
-        className="select select-bordered w-full max-w-xs"
+        className="select select-bordered w-full sm:w-auto"
         onChange={handleFilterChange}
         value={selectedFilter}
       >
