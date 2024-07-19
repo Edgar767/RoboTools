7edcd730f83c5561f67e6cd0a6cd5342ff674668
Edgar767 <utm20040079@utma.edu.mx>
2024-07-19 10:58:07 -0600

    Nuevos Cambios

--- src/Pages/RTSteamAcademy/RTSteamAcademy.jsx
+++ src/Pages/RTSteamAcademy/RTSteamAcademy.jsx
@@ -1,21 +1,47 @@
-import Prices from "../../Components/Prices";
-import Carrusel from "../../Components/Carrusel";
 import Floating from '../../Components/Animation/Floating';
-
-
-const images = ['/img/Imagenes_RTSteam/prueba1.png', '/img/Imagenes_RTSteam/prueba2.png', '/img/Imagenes_RTSteam/prueba3.png', '/img/Imagenes_RTSteam/prueba4.png', '/img/Imagenes_RTSteam/prueba5.png'];
 
 const RTSteamAcademy = () => {
 
   return (
     <div className="relative min-h-screen">
-      <Floating />
+        <Floating />
       <div className="relative z-10">
-    <div> {/* Eliminado el margen superior */}
-        <Carrusel images={images} />
-        <Prices />
-    </div>'
-    </div>
+        <h4 className="title color-variation-1 mt-8 mb-20 text-center text-5xl font-extrabold">LA MEJOR MANERA DE ENSEÑAR ROBOTICA</h4>
+        
+        <h4 className="title color-variation-1 mt-8 mb-20 text-center">
+          <span className="text-3xl font-extrabold">Dentro de RT STEAM Academy </span>
+          <span className="text-2xl font-semibold">sus profesores tendran herramientas necesarias para poner en accion interesantes y divertidos proyectos de robotica, tecnologia y programacion</span>
+        </h4>
+
+        <h4 className="title color-variation-1 mt-8 mb-20 text-center">
+          <span className="text-3xl font-extrabold">15 Años de Experiencia </span>
+          <span className="text-2xl font-semibold">en el diseño de programas, cursos y </span>
+          <span className="text-3xl font-extrabold">talleres educativos con enfoque STEAM</span>
+        </h4>
+
+        <h4 className="title color-variation-1 mt-8 mb-20 text-center text-5xl font-extrabold">CARACTERISTICAS DE RT STEAM ACADEMY</h4>
+        <div className=''>
+          <ul className="steps steps-vertical">
+            <li className="step step-primary">Rubricas de Evaluacion Basada en Competencias</li>
+            <li className="step step-secondary">Evaluaciones Parciales y Finales por grado escolar</li>
+            <li className="step step-accent">Motiva y mejora el engagment de tus alumnos otorgando insignias conforme van avanzando</li>
+            <li className="step step-error">Monitorea el avance y progreso de cada alumno y genera un reporte de su evaluacion</li>
+            <li className="step step-warning">Generador de constancias de meritos y logros ligado a las insignias obtenidas durante el ciclo escolar</li>
+          </ul>
+        </div>
+        
+          <h4 className="title color-variation-1 mt-8 mb-5 text-center text-5xl font-extrabold">¿Que encontraras en</h4>
+          <h4 className="title color-variation-1 mt-8 mb-20 text-center text-5xl font-extrabold">RT STEAM ACADEMY?</h4>
+          <div className=''>
+          <ul className="steps steps-vertical">
+            <li className="step step-primary">Sesiones paso a paso usando la Metodologia de las 5´C y el aprendizaje basado en proyectos (ABP)</li>
+            <li className="step step-secondary">Videos, Presentaciones y otras herramientas audiovisuales para contextualizar la clase</li>
+            <li className="step step-accent">Manuales de Construccion paso a paso</li>
+            <li className="step step-error">Programacion Ejemplo</li>
+            <li className="step step-warning">Retos y Desafios</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
