e9d3b2480bc01c5768201e614c5621753c9ddbc0
Edgar767 <utm20040079@utma.edu.mx>
2025-03-11 19:24:14 -0600

    Cambios menores

--- src/Pages/CursosRobotools/CursosRobotools.jsx
+++ src/Pages/CursosRobotools/CursosRobotools.jsx
@@ -56,13 +56,13 @@ const CursosRobotools = () => {
           <div className="container mx-auto max-w-4xl p-10 animate-on-scroll">
             <div className="w-full carousel rounded-box mb-20">
               <div className="w-full carousel-item image-zoom">
-                <img src="https://via.placeholder.com/1800x1000" alt="Carrusel 1" className="w-full" />
+                <img src="https://placehold.co/1800x1000" alt="Carrusel 1" className="w-full" />
               </div>
               <div className="w-full carousel-item image-zoom">
-                <img src="https://via.placeholder.com/1800x1000" alt="Carrusel 2" className="w-full" />
+                <img src="https://placehold.co/1800x1000" alt="Carrusel 2" className="w-full" />
               </div>
               <div className="w-full carousel-item image-zoom">
-                <img src="https://via.placeholder.com/1800x1000" alt="Carrusel 3" className="w-full" />
+                <img src="https://placehold.co/1800x1000" alt="Carrusel 3" className="w-full" />
               </div>
             </div>
           </div>
